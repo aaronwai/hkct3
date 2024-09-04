0484bbd72614d6e5d2f666f6d58f4a883e01e453
aaronwai <aaron.lung.wai@gmail.com>
2024-09-04 12:43:10 +0800

    Step 8 : create model files

--- bcre/settings.py
+++ bcre/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcre',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        "HOST": 'localhost',
     }
 }
 
